4084c5910858c0ce34188be9cfd2842ca4fe5de2
Joshua Baldwin <baldwinjd2@appstate.edu>
2021-07-23 19:35:59 -0400

    added sample players and updated css

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const exec = require("child_process").execFile;
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
@@ -19,11 +21,55 @@ app.get("/javascripts/generator.js", (req, res) => {
 
 app.get("/generate", (req, res) => {
   exec(__dirname + "/public/audio-generator.exe", function(err, data) {
-    console.log(err);
-    console.log(data);
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log(data);
+    }
   });
 })
 
+app.get("/sample0.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample0.wav");
+})
+
+app.get("/sample1.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample1.wav");
+})
+
+app.get("/sample2.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample2.wav");
+})
+
+app.get("/sample3.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample3.wav");
+})
+
+app.get("/sample4.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample4.wav");
+})
+
+app.get("/sample5.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample5.wav");
+})
+
+app.get("/sample6.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample6.wav");
+})
+
+app.get("/sample7.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample7.wav");
+})
+
+app.get("/sample8.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample8.wav");
+})
+
+app.get("/sample9.wav", (req, res) => {
+  res.sendFile(__dirname + "/sample9.wav");
+})
+
 app.listen(port, () => {
-  console.log("Listening at http://localhost:3000");
+  console.log("Listening at http://localhost:80");
 })
