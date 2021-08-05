354ebfa0f7281c6d0e867993dfaa87cecbdbf3b2
Joshua Baldwin <39449188+jbaldw@users.noreply.github.com>
2021-08-04 23:20:55 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const exec = require("child_process").execFile;
+const fs  = require("fs");
 const app = express();
 const port = process.env.PORT || 80;
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
+
+app.disable("view cache");
 
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
@@ -22,7 +25,6 @@ app.get("/javascripts/generator.js", (req, res) => {
 app.get("/generate", (req, res) => {
   exec(__dirname + "/public/audio-generator.exe", function(err, data) {
     
-
     if(err) {
       console.log(err);
     }
@@ -31,48 +33,49 @@ app.get("/generate", (req, res) => {
     }
 
     res.type(".js");
-    res.send("load.style.visibility = \"hidden\"");
+    res.send("file0.load(); file1.load(); file2.load(); file3.load(); file4.load(); \
+              file5.load(); file6.load(); file7.load(); file8.load(); file9.load(); load.style.visibility = \"hidden\"");
   });
 })
 
 app.get("/sample0.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample0.wav");
+  fs.createReadStream(__dirname + "/sample0.wav").pipe(res);
 })
 
 app.get("/sample1.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample1.wav");
+  fs.createReadStream(__dirname + "/sample1.wav").pipe(res);
 })
 
 app.get("/sample2.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample2.wav");
+  fs.createReadStream(__dirname + "/sample2.wav").pipe(res);
 })
 
 app.get("/sample3.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample3.wav");
+  fs.createReadStream(__dirname + "/sample3.wav").pipe(res);
 })
 
 app.get("/sample4.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample4.wav");
+  fs.createReadStream(__dirname + "/sample4.wav").pipe(res);
 })
 
 app.get("/sample5.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample5.wav");
+  fs.createReadStream(__dirname + "/sample5.wav").pipe(res);
 })
 
 app.get("/sample6.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample6.wav");
+  fs.createReadStream(__dirname + "/sample6.wav").pipe(res);
 })
 
 app.get("/sample7.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample7.wav");
+  fs.createReadStream(__dirname + "/sample7.wav").pipe(res);
 })
 
 app.get("/sample8.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample8.wav");
+  fs.createReadStream(__dirname + "/sample8.wav").pipe(res);
 })
 
 app.get("/sample9.wav", (req, res) => {
-  res.sendFile(__dirname + "/sample9.wav");
+  fs.createReadStream(__dirname + "/sample9.wav").pipe(res);
 })
 
 app.listen(port, () => {
